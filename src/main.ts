1c843ec046e731e25908e8a91547c5bcfe38c6b0
Fernando Urena <fernandourena98@gmail.com>
2026-06-28 14:33:47 -0400

    health check added

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { RpcCustomExceptionFilter } from './common';
 
 async function main() {
   const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', {
+    exclude: [{
+      path: '',
+      method: RequestMethod.GET,
+    }]
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -23,7 +28,7 @@ async function main() {
 
   app.useGlobalFilters(new RpcCustomExceptionFilter())
   await app.listen(envs.port ?? 3000);
-
+  console.log('health check working')
   logger.log(`Gateway running on port ${envs.port}`);
 }
 main();
